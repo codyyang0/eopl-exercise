71599078c112821c2a6086d98edbb2f4c11a2ab6
cody yang <cody.yang0@gmail.com>
2020-04-12 23:05:03 +0800

    delete compiled files

---
+++ chapter3/exercise3.11.scm
@@ -0,0 +1,5 @@
+;Exercise 3.11 [*] In a real language, one might have many operators such as those in
+;the preceding exercises. Rearrange the code in the interpreter so that it is easy to add
+;new operators.
+
+I don't know how to rearrange the code? just split into multi files?
